5130714f05ed7d607db04dc0044969c651aaf493
zbaker <zcbaker@ymail.com>
2019-06-07 17:39:58 -0700

    Corrected name reference to type reference
    Changed the name variable to the type variable in error printout.

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -51,7 +51,7 @@ namespace GradeBook.UserInterfaces
                 gradeBook = new RankedGradeBook(name);
             } else
             {
-                Console.WriteLine(name + " is not a supported type of gradebook, please try again");
+                Console.WriteLine(type + " is not a supported type of gradebook, please try again");
                 return;
             }
 
